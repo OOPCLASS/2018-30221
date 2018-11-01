7b473488b84e09082ae3243e85ae58c3dd0e81b3
Bogdan Paun <bogdan16paun@gmail.com>
2018-11-02 00:29:56 +0200

    Fixed mistakes

--- Students/Paun Bogdan/src/Lab03/BusTest.java
+++ Students/Paun Bogdan/src/Lab03/BusTest.java
@@ -9,13 +9,13 @@ public class BusTest {
 		
 		Bus[] busses = new Bus[10];
 		
-		Bus firstBus = new Bus("A", "RED");
-		Bus secondBus = new Bus("B", "GREEN");
-		Bus thirdBus = new Bus("C", "BLUE");
+		Bus firstBus = new Bus("A", "RED", "B-01-UNU");
+		Bus secondBus = new Bus("B", "GREEN", "C-02-DOI");
+		Bus thirdBus = new Bus("C", "BLUE", "D-03-TREI");
 
 		busses = new Bus[]{firstBus, secondBus, thirdBus};
 	
-		Station firstStation = new Station("Str. Baritiu", busses.length, busses);
+		Station firstStation = new Station("Str. Baritiu", busses);
 		
 		System.out.println("First Station details:");
 		firstStation.detailsToString();
@@ -31,13 +31,41 @@ public class BusTest {
 	
 	@Test
 	public void testBus() {
-		Bus firstBus = new Bus("M","RED");
+		Bus firstBus = new Bus("M","RED", "B-01-UNU");
 		System.out.println(firstBus.getBrand());
 		System.out.println(firstBus);
 		
-		Bus blueBus = new Bus("T", "BLUE");
-		blueBus.setLicensePlate("CJ 01 ABC");
+		Bus blueBus = new Bus("T", "BLUE", "C-02-DOI");
 		System.out.println(blueBus);
+		blueBus.setLicensePlate("CJ-01-ABC");
+		System.out.println(blueBus);
+	}
+
+	@Test
+	public void testAddRemoveBus() {
+		Bus firstBus = new Bus("M","RED", "CJ-01-UNU");
+		Bus secondBus = new Bus("T","BLUE", "CJ-02-DOI");
+		Bus thirdBus = new Bus("N","YELLOW", "CJ-03-TREI");
+		Bus fourthBus = new Bus("P","GREEN", "CJ-04-PATRU");
+		
+		System.out.println(firstBus);
+		System.out.println(secondBus);
+		System.out.println(thirdBus);
+		System.out.println(fourthBus);
+		
+		Station station = new Station();
+		
+		station.addBus(firstBus);
+		station.addBus(secondBus);
+		station.addBus(thirdBus);
+		station.addBus(fourthBus);
+		
+		station.detailsToString();	
+		
+		station.removeBus(firstBus);
+		
+		station.detailsToString();	
+		
 	}
 	
 }
--- Students/Paun Bogdan/src/Lab03/Station.java
+++ Students/Paun Bogdan/src/Lab03/Station.java
@@ -6,6 +6,7 @@ public class Station {
 	private int bussesCapacity;
 	private String address;
 	private String telephone;
+	private int noBusses;
 	
 	private String[] routes;
 	
@@ -14,50 +15,49 @@ public class Station {
 		this.address = new String("Str. Observatorului, nr. 34");
 		this.bussesCapacity = 10;
 		this.busses = new Bus[10];
+		this.noBusses = 0;
 	}
 	
-	public Station(String address, int bussesCapacity, Bus[] busses) {
-		this.bussesCapacity = bussesCapacity;
+	public Station(String address, Bus[] busses) {
+		this.bussesCapacity = busses.length;
 		this.address = address;
 		this.busses = busses;
+		this.noBusses = busses.length;
 	}
 	
+	public int getNoBusses() {
+		return noBusses;
+	}
+	public void setNoBusses(int noBusses) {
+		this.noBusses = noBusses;
+	}
 	public Bus[] getBusses() {
 		return busses;
 	}
-
 	public void setBusses(Bus[] busses) {
 		this.busses = busses;
 	}
-
 	public int getBussesCapacity() {
 		return bussesCapacity;
 	}
-
 	public void setBussesCapacity(int bussesCapacity) {
 		this.bussesCapacity = bussesCapacity;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-	
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
 	public String[] getRoutes() {
 		return routes;
 	}
-
 	public void setRoutes(String[] routes) {
 		this.routes = routes;
 	}
-	
 	public String getTelephone() {
 		return telephone;
 	}
-
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
@@ -67,20 +67,55 @@ public class Station {
 		System.out.println("Telephone: " + this.telephone);
 		System.out.println("Busses capacity: " + this.bussesCapacity);
 		System.out.println("Routes: " + this.routes);
-
-		boolean isEmpty = true;
 		
-		for(int i = 0; i < this.bussesCapacity; i++) {
-			if(this.busses[i] != null) {
-				System.out.println("Bus[" + i + "]: " + this.busses[i]);
-				isEmpty = false;
+		if(noBusses == 0) {
+			System.out.println("No busses available at the moment!");
+			return;
+		}
+		
+		for(int i = 0; i < this.noBusses; i++) {
+			System.out.println("Bus[" + (i+1) + "]: " + this.busses[i]);
+		}
+		
+	}
+	
+	public boolean addBus(Bus bus) {
+		if(noBusses < bussesCapacity) {
+			busses[noBusses] = bus;
+			noBusses++;
+			System.out.println("Bus <" + bus.getLicensePlate() + "> loaded.\n");
+			return true;
+		}
+		System.out.println("No empty spots! Try again later!\n");
+		return false;
+	}
+	
+	public boolean removeBus(Bus bus) {
+		
+		boolean busFound = false;
+		int i = 0;
+		
+		for(i = 0; i < noBusses; i++) {
+			if(busses[i].equals(bus)) {
+				busFound = true;
+				break;
 			}
 		}
 		
-		if(isEmpty) {
-			System.out.println("No busses available at the moment!");
+		int j;
+		
+		if(busFound) {
+			for(j = i; j < noBusses; j++) {
+				busses[j] = busses[j+1];
+			}
+			
+			noBusses--;
+			System.out.println("Bus <" + bus.getLicensePlate() + "> removed.\n");
+			return true;
 		}
-	
+		
+		System.out.println("Bus not found!\n");
+		return false;
 	}
 	
 }
